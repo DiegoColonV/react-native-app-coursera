57006167ed091f83a2b45c04c0da30a7dfa3f41d
Diego Colon <diego.colon3318@gmail.com>
2020-09-11 14:52:57 -0500

    Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
+
 
 class Contact extends Component {
 
     render() {
         
         return(
-            <Card title={'Contact Information'}>
+            <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>                
+            <Card title='Contact Information'>
                 <Text style={{margin: 10}}>121, Clear Water Bay Road</Text>
                 <Text style={{margin: 10}}>Clear Water Bay, Kowloon</Text>
                 <Text style={{margin: 10}}>HONG KONG</Text>
@@ -15,6 +18,7 @@ class Contact extends Component {
                 <Text style={{margin: 10}}>Fax: +852 8765 4321</Text>
                 <Text style={{margin: 10}}>Email:confusion@food.net</Text>
             </Card>
+            </Animatable.View>
         );
     }
 } 
